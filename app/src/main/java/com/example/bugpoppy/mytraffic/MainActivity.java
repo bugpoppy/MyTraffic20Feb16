399187d136a3152f1ab37dac9ffbb513f0476ef5
bugpoppy <bugpoppy@hotmail.com>
2016-02-20 15:08:36 +0700

    20feb16เสร็จแล้ว

--- app/src/main/java/com/example/bugpoppy/mytraffic/MainActivity.java
+++ app/src/main/java/com/example/bugpoppy/mytraffic/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.bugpoppy.mytraffic;
 
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ListView;
 
@@ -21,12 +25,31 @@ public class MainActivity extends AppCompatActivity {
         //bind widget คือการผูกตัวแปล
         bindwidget();
 
+     //button controler
+        buttonController();
     }// main method
+
+    private void buttonController() {
+       aboutButton.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View v) {
+               //sound effect
+               MediaPlayer mediaPlayer = MediaPlayer.create(getBaseContext(),R.raw.dog);
+               mediaPlayer.start();
+
+               //eb view
+               Intent intent = new Intent(Intent.ACTION_VIEW);
+               intent.setData(Uri.parse("https://youtu.be/Jy35oa5oFoY"));
+               startActivity(intent);
+           }
+       });
+
+    } //buttontroller
 
     private void bindwidget() {
 
         trafficListView = (ListView) findViewById(R.id.listView);
-        aboutButton = findViewById(R.id.button);
+        aboutButton = (Button) findViewById(R.id.button);
 
     } //bindwidget
 }// main class
